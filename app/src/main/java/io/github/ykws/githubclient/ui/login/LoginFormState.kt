1a364c4669a13f981d70d5e9b40cfc543b4adc8f
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2020-05-04 16:29:01 +0900

    :rocket: Creat a new Project
    Select a Project Templete Login Activity

---
+++ app/src/main/java/io/github/ykws/githubclient/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package io.github.ykws.githubclient.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
